13a26ef946d1d61910ddd0de5f2ce7b6b779c98f
Aditya Narayan Singh <richiaditya25@gmail.com>
2025-06-06 02:18:17 +0530

    .

--- ReactWithTS/src/App.tsx
+++ ReactWithTS/src/App.tsx
@@ -1,8 +1,38 @@
-let name: string;
-let age: number;
-
 function App() {
-  return <>Hello There!!</>;
+  let name: string;
+  let age: number;
+  let isStudent: boolean;
+  let hobbies: string[];
+  // tuple
+  let role: [number, string];
+  role = [2, "Adi"];
+
+  // to define types of objects
+
+  //we have 2 ways
+
+  // 1st way
+  let person: Object;
+
+  // 2nd way
+  //use type or interface
+
+  type Person = {
+    name: string;
+    age?: number;
+  };
+  let pperson: Person = {
+    name: "Aditya",
+    age: 12,
+  };
+
+  let lotsOfPeople: Person[];
+
+  return (
+    <>
+      <div>Hello World!!</div>
+    </>
+  );
 }
 
 export default App;
