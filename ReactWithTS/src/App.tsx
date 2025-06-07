d38f4cf56a41775d43cce0cf8a71d16ef7600035
Aditya Narayan Singh <richiaditya25@gmail.com>
2025-06-07 23:54:22 +0530

    type and interface

--- ReactWithTS/src/App.tsx
+++ ReactWithTS/src/App.tsx
@@ -7,6 +7,7 @@ function App() {
   let role: [number, string];
   role = [2, "Adi"];
 
+  // -----------------------------------------------------
   // to define types of objects
 
   //we have 2 ways
@@ -27,6 +28,65 @@ function App() {
   };
 
   let lotsOfPeople: Person[];
+
+  let agee: number | string; // can be a number or string
+  const newLocal = (agee = 7);
+
+  // instead of any use unknown
+
+  let a: any;
+  let b: unknown;
+
+  // -----------------------------------------------------
+  //Functions
+
+  printName: Function;
+  function printName(name: string) {
+    console.log(name);
+  }
+  printName("Adiyya");
+
+  // 2nd method
+
+  let printNamee: (name: string) => void; // as it returns noting it just prints. We can use never also.(name:string)=>never is a function never returns anyting
+  // void means it can return undefined.
+
+  // deep dive in Aliases
+
+  type Personn = {
+    name: string;
+    age: number;
+  };
+  interface Personnn {
+    name: string;
+    age: string;
+  }
+
+  // but whats the difference between type and interface?
+
+  type X = {
+    a: string;
+    b: number;
+  };
+  type Y = {
+    c: string;
+    d: number;
+  };
+
+  type Z = X &
+    Y & {
+      e: string;
+      f: number;
+    };
+
+  let z: Z = {
+    a: "Adi",
+    b: 1,
+    c: "cosmo",
+    d: 23,
+    e: "sw",
+    f: 23,
+  };
 
   return (
     <>
